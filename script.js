9987ceede9060436ab266caa19799356e95857f8
Sofia <100186523+iamsofiaangela@users.noreply.github.com>
2025-07-31 04:11:49 +0800

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-const roles = ["UI Designer", "Web Developer", "Creative Freelancer"];
-let i = 0;
-let char = 0;
-let isDeleting = false;
-let currentText = '';
-let typingElement = document.getElementById('typing');
-
-function typeEffect() {
-  const fullText = roles[i];
-
-  if (isDeleting) {
-    currentText = fullText.substring(0, char--);
-  } else {
-    currentText = fullText.substring(0, char++);
-  }
-
-  typingElement.innerHTML = currentText;
-
-  if (!isDeleting && char === fullText.length) {
-    isDeleting = true;
-    setTimeout(typeEffect, 1000);
-  } else if (isDeleting && char === 0) {
-    isDeleting = false;
-    i = (i + 1) % roles.length;
-    setTimeout(typeEffect, 200);
-  } else {
-    setTimeout(typeEffect, isDeleting ? 50 : 100);
-  }
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("active");
 }
-
-typeEffect();
