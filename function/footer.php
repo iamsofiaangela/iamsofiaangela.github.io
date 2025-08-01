ff6e4d1c104b1334b41e7f924433dd30f0bced89
Sofia <100186523+iamsofiaangela@users.noreply.github.com>
2025-08-01 22:13:52 +0800

    Update footer.php

--- function/footer.php
+++ function/footer.php
@@ -1,127 +1,50 @@
-<!--- FOOTER --->
+<!-- FOOTER FOR PORTFOLIO -->
 <footer class="bg-dark-gradient footer">
-    <div class="footer-top">
-        <div class="container">
-            <div class="footer-border-bottom pb-6 mb-5">
-                <div class="row">
-                    <div class="col-lg-4 col-xl-3">
-                    </div>
-                    <div class="col-lg-4 col-xl-6">
-                    </div>
-                    <div class="col-lg-4 col-xl-3">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 col-lg-3 my-4">
-                    <div class="mb-4">
-                        
-                    </div>
-                    <div class="text-white-65 mb-4 text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</div>
-                    <div class="nav footer-socila-icon">
-                        <a href="#">
-<i class="fab fa-facebook-f"></i>
-</a>
-                        <a href="#">
-<i class="fab fa-twitter"></i>
-</a>
-                        <a href="#">
-<i class="fab fa-instagram"></i>
-</a>
-                        <a href="#">
-<i class="fab fa-linkedin-in"></i>
-</a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-2 my-4">
-                    <h5 class="text-white h6 mb-4">Customer</h5>
-                    <ul class="list-unstyled white-link footer-links">
-                        <li>
-                            <a href="#">Get Corpo</a>
-                        </li>
-                        <li>
-                            <a href="#">Documentation</a>
-                        </li>
-                        <li>
-                            <a href="#">Help center</a>
-                        </li>
-                        <li>
-                            <a href="#">Pricing</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact us</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-lg-2 my-4">
-                    <h5 class="text-white h6 mb-4">Product</h5>
-                    <ul class="list-unstyled white-link footer-links">
-                        <li>
-                            <a href="#">Portfolio</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#">Join our team</a>
-                        </li>
-                        <li>
-                            <a href="#">Elements</a>
-                        </li>
-                        <li>
-                            <a href="#">Products</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-lg-2 my-4">
-                    <h5 class="text-white h6 mb-4">Company</h5>
-                    <ul class="list-unstyled white-link footer-links">
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Careers</a>
-                        </li>
-                        <li>
-                            <a href="#">Legal &amp; Security</a>
-                        </li>
-                        <li>
-                            <a href="#">Terms of use</a>
-                        </li>
-                        <li>
-                            <a href="#">We're hiring!</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-lg-3 my-4">
-                    <h5 class="text-white h6 mb-4">Company</h5>
-                    <div class="white-link">
-                        <div class="d-flex pb-3">
-                            <div class="avatar avatar-lg rounded">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" title="" alt="">
-                            </div>
-                            <div class="col ps-3">
-                                <small class="text-white-65">29 March 2021</small>
-                                <h6 class="font-w-500 h6 m-0"><a class="text-white" href="#">Markdown Language Sample Blog Post Styling</a></h6>
-                            </div>
-                        </div>
-                        <div class="d-flex">
-                            <div class="avatar avatar-lg rounded">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" title="" alt="">
-                            </div>
-                            <div class="col ps-3">
-                                <small class="text-white-65">29 March 2021</small>
-                                <h6 class="font-w-500 h6 m-0"><a class="text-white" href="#">Markdown Language Sample Blog Post Styling</a></h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <!-- About Me / Intro -->
+        <div class="col-md-4 my-4">
+          <h5 class="text-white h6 mb-3">About Me</h5>
+          <p class="text-white-65">
+            I'm Fia — a creative developer and designer passionate about building functional, beautiful digital products. 
+          </p>
         </div>
-    </div>
-    <div class="footer-bottom footer-border-top light py-3">
-        <div class="container text-center">
-            <p>© <span id="year"></span> Fia. All rights reserved.<a href="https://iamsofiaangela.github.io" class="text-reset text-white">https://iamsofiaangela.github.io</a></p>
+
+        <!-- Quick Links -->
+        <div class="col-md-4 my-4">
+          <h5 class="text-white h6 mb-3">Quick Links</h5>
+          <ul class="list-unstyled white-link footer-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="resume.pdf" target="_blank">Resume</a></li>
+          </ul>
         </div>
+
+        <!-- Social Links -->
+        <div class="col-md-4 my-4">
+          <h5 class="text-white h6 mb-3">Follow Me</h5>
+          <div class="nav footer-socila-icon">
+            <a href="https://facebook.com/yourusername" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com/yourusername" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://instagram.com/yourusername" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://linkedin.com/in/yourusername" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+
+      </div>
     </div>
+  </div>
+
+  <!-- Bottom Footer -->
+  <div class="footer-bottom footer-border-top light py-3">
+    <div class="container text-center">
+      <p>
+        © <span id="year"></span> Fia. All rights reserved.
+        <a href="https://iamsofiaangela.github.io" class="text-reset text-white" target="_blank">Portfolio Site</a>
+      </p>
+    </div>
+  </div>
 </footer>
